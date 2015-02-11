5d2e6c0f46974339699d56134353549a223dc30d
Travis Hoover <thoov7@gmail.com>
2015-02-11 11:14:09 -0800

    Move deactivate logic to resetController
    In order to address an issue where a transition from a dynamic route to
    the same route with a different dynamic segment: IE: room/1 to room/2
    never closed the socket.

---
+++ tests/dummy/app/routes/sockets/dynamic.js
@@ -0,0 +1,20 @@
+import Ember from 'ember';
+import socketMixin from 'ember-websockets/mixins/sockets';
+
+export default Ember.Route.extend(socketMixin, {
+
+    socketURL: null,
+
+    updateSocketURL: function(roomID) {
+        this.set('socketURL', 'ws://localhost:8080/room/%@'.fmt(roomID));
+    },
+
+    model: function(params) {
+        return {id: params.room_id};
+    },
+
+    setupController: function(controller, model) {
+        this.updateSocketURL(model.id);
+        this._super.apply(this, arguments);
+    }
+});
