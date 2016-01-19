1075e05b67cc27748fa641acb77ab9232f6ba565
Travis Hoover <thoov7@gmail.com>
2016-01-19 00:17:07 -0800

    Upgrade to ember-cli 1.13.14

--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -1,18 +1,11 @@
 import Ember from 'ember';
 import config from './config/environment';
 
-var Router = Ember.Router.extend({
+const Router = Ember.Router.extend({
   location: config.locationType
 });
 
 Router.map(function() {
-  this.resource('sockets', function() {
-    this.route('example');
-  });
-
-  this.resource('socketio', function() {
-    this.route('simple');
-  });
 });
 
 export default Router;
