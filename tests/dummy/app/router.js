11d02345cbc5262f9461ee0317db93bf308bd29f
Travis Hoover <thoov7@gmail.com>
2016-10-05 13:51:22 -0700

    Updating project to 2.8

--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -2,11 +2,12 @@ import Ember from 'ember';
 import config from './config/environment';
 
 const Router = Ember.Router.extend({
-  location: config.locationType
+  location: config.locationType,
+  rootURL: config.rootURL
 });
 
 Router.map(function() {
-   this.route('sockets', function() {
+  this.route('sockets', function() {
     this.route('example');
   });
 
