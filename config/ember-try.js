c846b326da07c918f8dc9aaadc3d060c1335a1fc
Travis Hoover <thoov7@gmail.com>
2017-10-25 15:42:03 -0700

    Removing default try case

--- config/ember-try.js
+++ config/ember-try.js
@@ -73,12 +73,6 @@ module.exports = {
           'ember-source': null
         }
       }
-    },
-    {
-      name: 'ember-default',
-      npm: {
-        devDependencies: {}
-      }
     }
   ]
 };
