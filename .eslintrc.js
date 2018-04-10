a5bb32d123ad72a3bd09c3b4d89e6acce46fa4a4
Travis Hoover <thoov7@gmail.com>
2018-04-09 22:18:49 -0700

    Ember 3 init

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,13 @@ module.exports = {
     ecmaVersion: 2017,
     sourceType: 'module'
   },
-  extends: 'eslint:recommended',
+  plugins: [
+    'ember'
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:ember/recommended'
+  ],
   env: {
     browser: true
   },
@@ -13,5 +19,34 @@ module.exports = {
   globals: {
     io: true,
     URI: true
-  }
+  },
+  overrides: [
+    // node files
+    {
+      files: [
+        'index.js',
+        'testem.js',
+        'ember-cli-build.js',
+        'config/**/*.js',
+        'tests/dummy/config/**/*.js'
+      ],
+      excludedFiles: [
+        'app/**',
+        'addon/**',
+        'tests/dummy/app/**'
+      ],
+      parserOptions: {
+        sourceType: 'script',
+        ecmaVersion: 2015
+      },
+      env: {
+        browser: false,
+        node: true
+      },
+      plugins: ['node'],
+      rules: Object.assign({}, require('eslint-plugin-node').configs.recommended.rules, {
+        // add your custom rules and overrides for node files here
+      })
+    }
+  ]
 };
--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 /* eslint-env node */
+'use strict';
 
 // To use it create some files under `mocks/`
 // e.g. `server/mocks/ember-hamsters.js`
@@ -10,14 +11,14 @@
 // };
 
 module.exports = function(app) {
-  const globSync = require('glob').sync;
-  const mocks = globSync('./mocks/**/*.js', { cwd: __dirname }).map(require);
-  const proxies = globSync('./proxies/**/*.js', { cwd: __dirname }).map(require);
+  const globSync   = require('glob').sync;
+  const mocks      = globSync('./mocks/**/*.js', { cwd: __dirname }).map(require);
+  const proxies    = globSync('./proxies/**/*.js', { cwd: __dirname }).map(require);
 
   // Log proxy requests
   const morgan = require('morgan');
   app.use(morgan('dev'));
 
-  mocks.forEach(function(route) { route(app); });
-  proxies.forEach(function(route) { route(app); });
+  mocks.forEach(route => route(app));
+  proxies.forEach(route => route(app));
 };
